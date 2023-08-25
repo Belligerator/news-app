f686ae47d0833d109c4bb57a76ac8c9b01380eb8
Dmitrij Buckovsky <dmitrij.buckovsky@initedsolutions.cz>
2023-08-25 16:02:51 +0200

    API url changed

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,3 @@
-const kBaseUrl = 'https://news-java.belligerator.cz';
+const kBaseUrl = 'https://news.belligerator.cz';
 const kApiUrl = '$kBaseUrl/api';
 const kBasicAuth = 'YmVsbGlnZXJhdG9yOkhlc2xvMTIz';
